82c51706eb70357107a22945ebac4f3a05e50df6
thih-n <dongnguyen1968hldh@gmail.com>
2024-08-30 16:51:52 +0700

    gggg

--- FPIC/fontend/fpic/src/App.js
+++ FPIC/fontend/fpic/src/App.js
@@ -3,12 +3,15 @@ import axios from "axios";
 import ZoomableImage from "./ZoomableImage";
 import "./App.css";
 import CustomButtonGroup from "./ButtonColor";
+import Header from "./elements/Header";
+import { Col, Row, Button, Card } from "react-bootstrap";
 
 function App() {
   const [images, setImages] = useState([]);
-  const [showAll, setShowAll] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1); // Trang hiện tại
+  const [imagesPerPage] = useState(24); // Số lượng ảnh mỗi trang
   const [selectedImageIndex, setSelectedImageIndex] = useState(null);
-  const [resetKey, setResetKey] = useState(0); // State để lưu key cho ZoomableImage
+  const [resetKey, setResetKey] = useState(0); // Reset ảnh khi chuyển ảnh
   const [fileData, setFileData] = useState(null);
 
   useEffect(() => {
@@ -22,27 +25,26 @@ function App() {
       });
   }, []);
 
+  // Tính toán vị trí bắt đầu và kết thúc của ảnh cho trang hiện tại
+  const indexOfLastImage = currentPage * imagesPerPage;
+  const indexOfFirstImage = indexOfLastImage - imagesPerPage;
+  const currentImages = images.slice(indexOfFirstImage, indexOfLastImage);
+
   const handleImageClick = (index) => {
     setSelectedImageIndex(index);
-    setResetKey((prevKey) => prevKey + 1); // Tăng giá trị key khi ảnh thay đổi
+    setResetKey((prevKey) => prevKey + 1); // Tăng key để reset ZoomableImage khi ảnh thay đổi
   };
 
-  const toggleShowAll = () => {
-    setShowAll(!showAll);
+  const showPreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
   };
 
-  const showPreviousImage = () => {
-    setSelectedImageIndex((prevIndex) =>
-      prevIndex > 0 ? prevIndex - 1 : images.length - 1
-    );
-    setResetKey((prevKey) => prevKey + 1); // Reset ảnh khi chuyển ảnh
-  };
-
-  const showNextImage = () => {
-    setSelectedImageIndex((prevIndex) =>
-      prevIndex < images.length - 1 ? prevIndex + 1 : 0
-    );
-    setResetKey((prevKey) => prevKey + 1); // Reset ảnh khi chuyển ảnh
+  const showNextPage = () => {
+    if (currentPage < Math.ceil(images.length / imagesPerPage)) {
+      setCurrentPage(currentPage + 1);
+    }
   };
 
   const selectedImage = images[selectedImageIndex];
@@ -63,141 +65,140 @@ function App() {
   }, [selectedImage]);
 
   return (
-    <div className="App">
-      <div className="d-flex flex-wrap justify-content-center">
-        {!showAll &&
-          images.slice(0, 30).map((image, index) => (
-            <a
+    <div className="bg-image">
+      <Header />
+      <div className="App">
+        <div className="image-grid">
+          {currentImages.map((image, index) => (
+            <Card
               key={index}
-              onClick={() => handleImageClick(index)}
+              className="m-2"
+              style={{ cursor: "pointer" }}
+              onClick={() => handleImageClick(indexOfFirstImage + index)}
               data-bs-toggle="modal"
               data-bs-target="#imageModal"
-              className="img-thumbnail"
             >
-              <img
+              <Card.Img
+                variant="top"
                 src={image?.img1}
                 alt={image?.name}
-                className="img-fluid rounded m-3"
-                style={{ cursor: "pointer", width: "150px" }}
+                style={{ width: "100%", height: "150px", objectFit: "cover" }}
               />
-            </a>
+              <Card.Body>
+                <Card.Title>{image?.name}</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title.
+                </Card.Text>
+              </Card.Body>
+            </Card>
           ))}
+        </div>
 
-        {showAll &&
-          images.map((image, index) => (
-            <a
-              key={index}
-              onClick={() => handleImageClick(index)}
-              data-bs-toggle="modal"
-              data-bs-target="#imageModal"
-              className="img-thumbnail"
-            >
-              <img
-                src={image?.img1}
-                alt={image?.name}
-                className="img-fluid rounded m-3"
-                style={{ cursor: "pointer", width: "150px" }}
-              />
-            </a>
-          ))}
-      </div>
+        {/* Phân trang */}
+        <Row className="pagination">
+          <Button
+            onClick={showPreviousPage}
+            disabled={currentPage === 1}
+            className="btn"
+            style={{ width: 150 }}
+          >
+            Trang trước
+          </Button>
+          <span>
+            Trang
+            <input
+              type="text"
+              value={currentPage}
+              onChange={(e) => setCurrentPage(Number(e.target.value))}
+              onBlur={() => {
+                if (currentPage < 1) setCurrentPage(1);
+                if (currentPage > Math.ceil(images.length / imagesPerPage))
+                  setCurrentPage(Math.ceil(images.length / imagesPerPage));
+              }}
+              disabled
+            />
+            / {Math.ceil(images.length / imagesPerPage)}
+          </span>
+          <Button
+            onClick={showNextPage}
+            disabled={currentPage === Math.ceil(images.length / imagesPerPage)}
+            className="btn"
+            style={{ width: 150 }}
+          >
+            Trang sau
+          </Button>
+        </Row>
 
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <button className="btn btn-primary mt-3" onClick={toggleShowAll}>
-          {showAll ? "Thu gọn" : "Xem thêm"}
-        </button>
-      </div>
+        {/* Modal hiển thị ảnh */}
+        <div
+          className="modal fade"
+          id="imageModal"
+          tabIndex="-1"
+          aria-labelledby="staticBackdropLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog modal-fullscreen p-5 pb-0">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="staticBackdropLabel">
+                  {selectedImage?.name}
+                </h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body row">
+                <Col xs={10}>
+                  {selectedImage && (
+                    <div className="d-flex flex-column">
+                      <div className="d-flex justify-content-center align-items-center mb-3">
+                        <ZoomableImage key={resetKey} data={selectedImage} />
+                      </div>
 
-      <div
-        className="modal fade"
-        id="imageModal"
-        tabIndex="-1"
-        aria-labelledby="staticBackdropLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog modal-fullscreen p-5 pb-0">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="staticBackdropLabel">
-                {selectedImage?.name}
-              </h5>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div className="modal-body row">
-              <div className="col-10">
-                {selectedImage && (
-                  <div className="d-flex flex-column">
-                    <div className="d-flex justify-content-center align-items-center mb-3">
-                      <button
-                        className="btn btn-secondary me-3"
-                        onClick={showPreviousImage}
-                        disabled={images.length === 0}
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="16"
-                          fill="currentColor"
-                          className="bi bi-caret-left-fill"
-                          viewBox="0 0 16 16"
-                        >
-                          <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z" />
-                        </svg>
-                      </button>
-                      <ZoomableImage key={resetKey} data={selectedImage} />
-                      <button
-                        className="btn btn-secondary ms-3"
-                        onClick={showNextImage}
-                        disabled={images.length === 0}
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="16"
-                          fill="currentColor"
-                          className="bi bi-caret-right-fill"
-                          viewBox="0 0 16 16"
-                        >
-                          <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
-                        </svg>
-                      </button>
-                    </div>
-
-                    <div className="w-100 mt-4">
-                      <h6>More images:</h6>
-                      <div className="d-flex flex-wrap justify-content-start">
-                        {images
-                          .filter((img, index) => index !== selectedImageIndex)
-                          .slice(0, 19)
-                          .map((image, index) => (
-                            <a
-                              key={index}
-                              onClick={() =>
-                                handleImageClick(images.indexOf(image))
-                              }
-                              className="img-thumbnail m-2"
-                              data-bs-target="#imageModal"
-                            >
-                              <img
-                                src={image?.img1}
-                                alt={image?.name}
-                                className="img-fluid rounded"
-                                style={{ cursor: "pointer", width: "100px" }}
-                              />
-                            </a>
-                          ))}
+                      <div className="w-100 mt-4">
+                        <h6>More images:</h6>
+                        <div className="d-flex flex-wrap justify-content-start">
+                          {images
+                            .filter(
+                              (img, index) => index !== selectedImageIndex
+                            )
+                            .slice(0, 19)
+                            .map((image, index) => (
+                              <Card
+                                key={index}
+                                className="m-2"
+                                style={{ width: "100px", cursor: "pointer" }}
+                                onClick={() =>
+                                  handleImageClick(images.indexOf(image))
+                                }
+                                data-bs-target="#imageModal"
+                              >
+                                <Card.Img
+                                  variant="top"
+                                  src={image?.img1}
+                                  alt={image?.name}
+                                  style={{ width: "100%" }}
+                                />
+                                <Card.Body>
+                                  <Card.Title>{image?.name}</Card.Title>
+                                  <Card.Text>
+                                    Some quick example text to build on the card
+                                    title.
+                                  </Card.Text>
+                                </Card.Body>
+                              </Card>
+                            ))}
+                        </div>
                       </div>
                     </div>
-                  </div>
-                )}
-              </div>
-              <div className="col-2">
-                <CustomButtonGroup fileData={fileData} />
+                  )}
+                </Col>
+                <Col xs={2}>
+                  <CustomButtonGroup fileData={fileData} />
+                </Col>
               </div>
             </div>
           </div>
